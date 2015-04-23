db2d836ce57863d0a6639fe26597d0d2afeb030e
Chris Ramirez <ramchris036@yahoo.com>
2015-04-23 23:57:30 +0000

    finished graphmaker?

--- unfinishedgraphmaker.py
+++ unfinishedgraphmaker.py
@@ -2,23 +2,36 @@
 
 import sys
 
-if len(sys.argv) == 2:
+""" if len(sys.argv) == 2:
     user_input = sys.argv(1)
 else:
     print "Sorry, we need a set of playlists to go through"
     sys.exit()
+"""
+
+user_input = [['a','b','c','d'],['d','e','f'],['a','d']]
 
 output_graph = {}
 current_edge = {}
 
 for playlist in user_input:
     for song in playlist:
-        if song in output:
-            continue
+        if song in output_graph:
+            for other_song in playlist:
+                if other_song == song:
+                    continue
+                elif other_song in output_graph[song]:
+                    new_v = output_graph[song][other_song]
+                    new_v = (1.0/new_v) + 1
+                    new_v = 1.0/new_v
+                    output_graph[song][other_song] = new_v
+                else: 
+                    output_graph[song][other_song] = 1.0
         else:
             output_graph[song] = {}
             for other_song in playlist:
                 if other_song == song:
                     continue
                 else:
-                    output_graph[song][other_song]= 1
+                    output_graph[song][other_song] = 1.0
+print output_graph
