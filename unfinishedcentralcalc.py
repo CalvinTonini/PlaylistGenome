cb9271801f6d4bb6e81508ae2808dabd55d2807a
Chris Ramirez <ramchris036@yahoo.com>
2015-04-24 16:34:49 +0000

    finished centralcalc?

--- unfinishedcentralcalc.py
+++ unfinishedcentralcalc.py
@@ -15,12 +15,15 @@ if len(sys.argv) != 3:
 
 centrality = [0.0] * len(dist)
 
-for value in centrality:
-    for i, song in enumerate(dist):
-        new_value = 0.0
-        for j, edge in enumerate(dist[i]):
+for i, song in enumerate(dist):
+    new_value = 0.0
+    for j, edge in enumerate(dist[i]):
+        if dist[i][j] == float("inf"):
+            continue
+        else:
             new_value = new_value + dist[i][j]
-        centrality[i] = (1.0/new_value)
-
+    centrality[i] = (1.0/new_value)
+print songlist
+print centrality
 thing = centrality.index(max(centrality))
 print songlist[thing]
