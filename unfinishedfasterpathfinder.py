99736aa87640612eb7687802f68b20a8896defef
Bianca Trombetta <btrombetta@college.harvard.edu>
2015-04-30 19:14:25 +0000

    minor edits

--- unfinishedfasterpathfinder.py
+++ unfinishedfasterpathfinder.py
@@ -11,7 +11,7 @@ import pickle
 if len(sys.argv) == 2:
     user_input = sys.argv[1]
 else:
-    print "usage: python unfinishedpathfinder.py [graph.csv]"
+    print "usage: python unfinishedpathfinder.py [graph.pyfile]"
     sys.exit()
 
 dist = []
