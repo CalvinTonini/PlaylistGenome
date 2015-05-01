966ad95f8f310190a2948c2d8c99d54211698a0f
Calvin Tonini <CalvinTonini@users.noreply.github.com>
2015-05-01 08:49:15 +0000

    Triangles as sets, with playlist check

--- triangles.py
+++ triangles.py
@@ -4,20 +4,41 @@
 import sys
 import pickle
 
-if len(sys.argv) == 3:
+if len(sys.argv) == 4:
     user_input = sys.argv[1]
-    song = sys.argv[2]
+    song = sys.argv[3]
+    user_playlists = sys.argv[2]
 else:
-    print "usage: python triangles.py graph.pyfile 'song'"
+    print "usage: python triangles.py graph.pyfile playlists.pyfile 'song'"
     sys.exit()
 
 picklefile = open(user_input, 'rb')
 user_graph = pickle.load(picklefile)
 picklefile.close()
+picklefile = open(user_playlists, 'rb')
+playlists = pickle.load(picklefile)
+picklefile.close()
 
-output = []
+output = set()
 for other_song in user_graph[song]:
     for other_songs_song in user_graph[other_song]:
         if other_songs_song in user_graph[song]:
-            output.append([song, other_song, other_songs_song])
-return output
+            triangle = frozenset([other_songs_song, song, other_song])
+            flag = False
+            for playlist in playlists:
+                if triangle.issubset(set(playlist)):
+                    flag = True
+            if flag == False:
+                output.add(triangle)
+# for song in user_graph:
+#     for other_song in user_graph[song]:
+#         for other_songs_song in user_graph[other_song]:
+#             if other_songs_song in user_graph[song]:
+#                 triangle = frozenset([other_songs_song, song, other_song])
+#                 flag = False
+#                 for playlist in playlists:
+#                     if triangle.issubset(set(playlist)):
+#                         flag = True
+#                 if flag == False:
+#                     output.add(triangle)
+print output
